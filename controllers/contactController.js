4ea842f2e83147f3d12303a52d4fbba15ec5945c
Ain Mor <ainbmor@gmail.com>
2024-06-01 12:00:40 +0200

    added changes for contact form

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,7 +1,25 @@
 const petsCollection = require("../db").db().collection("pets")
 const sanitizeHtml = require("sanitize-html")
 const sgMail = require('@sendgrid/mail')
+const { SMTPServer } = require('smtp-server');
+const simpleParser = require('mailparser').simpleParser;
+const validator = require('validator');
 const { ObjectId } = require("mongodb")
+const fs = require('fs');
+
+// Read the private key and certificate
+const privateKey = fs.readFileSync('/Users/ainmor/private-key.pem', 'utf8');
+const certificate = fs.readFileSync('/Users/ainmor/cert.pem', 'utf8');
+
+const credentials = { key: privateKey, cert: certificate };
+const passphrase = process.env.PASSWORD_PHRASE; // Provide the correct passphrase here
+
+const certOptions = {
+   secure: true,
+   key: credentials.key,
+   cert: credentials.cert,
+   passphrase: passphrase, // Provide the correct passphrase when loading the private key
+};
 
 
 const sanitizeOptions = {
@@ -11,10 +29,16 @@ const sanitizeOptions = {
  
 
 exports.submitContact = async function (req, res) {
-   if (req.body.secret.toUpperCase() !== "MAGIC") {
+   if (req.body.secret.toUpperCase() !== "PUPPY") {
       console.log("spam detected");
       return res.json({error: "error due to spam detected"});
    }
+
+   if (!validator.isEmail(req.body.email)) {
+      console.log("invalid email detected");
+      return res.json({error: "error due to invalid email detected"});
+   }
+
 
    // invalid id detection
    if (!ObjectId.isValid(req.body.petId)) {
@@ -36,8 +60,49 @@ exports.submitContact = async function (req, res) {
       comment: sanitizeHtml(req.body.comment, sanitizeOptions)
    }
 
-   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-   const msg = {
+   // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+
+   const server = new SMTPServer({
+       // Enable TLS
+      secure: true, 
+      ...certOptions, 
+      authOptional: true,
+      onData(stream, session, callback) {
+         stream.on('data', (chunk) => {
+             console.log('Message chunk:', chunk.toString());
+         });
+ 
+         stream.on('end', () => {
+             console.log('Message received');
+             callback(null, // Error first callback, null means successful
+                 'Message received and processed');
+         });
+     },
+   });
+
+   server.listen(465, () => {
+      console.log('SMTP Server is running on port 465');
+  });
+
+  const nodemailer = require('nodemailer');
+  const transporter = nodemailer.createTransport({
+      service: 'Gmail', // Use your email service provider
+      auth: {
+         user: process.env.USER_NAME,
+         pass: process.env.PASSWORD // Use an app password generated from your Google account
+      }, 
+      port: 465,
+      secure: true, // Use TLS
+      tls: {
+         // Do not fail on invalid certs
+         rejectUnauthorized: false
+     }
+  });
+
+   console.log("finalObject email: ", finalObject.email);
+
+   const mailOptions = {
       to: finalObject.email,
       from: 'ainbmor@gmail.com',
       subject: `Thank you for your interest in our pet ${pet.name}.`,
@@ -47,20 +112,36 @@ exports.submitContact = async function (req, res) {
              <p>Below is the email that you send us for the record purpose.</p>
              <p><em>${finalObject.comment}</em></p><br><p>Best regards, Ain Mor</p>`,
    };
+
    try {
-      await sgMail.send(msg);
-    } catch (error) {
+      transporter.sendMail(mailOptions, (error, info) => {
+         if (error) {
+            return console.log(`Error: ${error}`);
+      }
+      console.log('Email sent: ' + info.response);
+   });
+   } catch (error) {
       console.error(error);
   
       if (error.response) {
         console.error(error.response.body)
-      }
-    }   
+      }      
+   }  
+
+   // try {
+   //    await sgMail.send(msg);
+   //  } catch (error) {
+   //    console.error(error);
+  
+   //    if (error.response) {
+   //      console.error(error.response.body)
+   //    }
+   //  }   
 
     const msg_to_owner = {
       to: 'ainbmor@gmail.com',
       from: 'ainbmor@gmail.com',
-      subject: `Thank you for your interest in our pet ${pet.name}.`,
+      subject: `You have an email who is interested on the pet ${pet.name}.`,
       text: finalObject.comment,
       html: `<h3 style="color:blue;">You have got the customer who is interested on the pet ${pet.name}.</h3><br><p>Kindly make an appointment to the 
             customer <strong>${finalObject.email}</strong> and inform the sales department immediately to contact the customer for further process.</p>
@@ -68,15 +149,32 @@ exports.submitContact = async function (req, res) {
              <p><em>${finalObject.comment}</em></p><br><p>Best regards, Ain Mor</p>`,
    };
 
-   try {
-      await sgMail.send(msg_to_owner);
-    } catch (error) {
+   // try {
+   //    await sgMail.send(msg_to_owner);
+   //  } catch (error) {
+   //    console.error(error);
+  
+   //    if (error.response) {
+   //      console.error(error.response.body)
+   //    }
+   //  } 
+
+   try   {
+      transporter.sendMail(msg_to_owner, (error, info) => {
+         if (error) {
+            return console.log(`Error: ${error}`);
+        }
+        console.log('Email sent: ' + info.response);
+     });
+   }
+   catch (error) {
       console.error(error);
   
       if (error.response) {
         console.error(error.response.body)
       }
-    } 
+   }
+   
 
    console.log("submitContact: ",finalObject);
    res.send("thanks for contacting us!  We will get back to you shortly.");
